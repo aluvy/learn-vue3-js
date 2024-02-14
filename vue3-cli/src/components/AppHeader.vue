282bea879850c0a122323afe25b939f107567493
songmiok <aluvy_@naver.com>
2024-02-14 11:00:32 +0900

    Create AppHeader.vue

---
+++ vue3-cli/src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ appTitle }}</h1>
+    <button type="button" @click="changeTitle">change</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    appTitle: String
+  },
+  methods: {
+    changeTitle() {
+      this.$emit('changeTitle');
+    }
+  }
+}
+</script>
