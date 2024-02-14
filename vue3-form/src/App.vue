655ebff5a08643bcf4410ebe62a9a6fa43f90d6a
songmiok <aluvy_@naver.com>
2024-02-14 13:38:19 +0900

    Update App.vue

--- vue3-form/src/App.vue
+++ vue3-form/src/App.vue
@@ -17,30 +17,56 @@
 
 <script>
 import axios from 'axios';
+import { ref } from 'vue';
 
 export default {
-  data() {
-    return {
-      userId: '',
-      password: '',
-      data: [],
-    }
-  },
-  methods: {
-    async submit() {
-      try {
-        const data = {
-          userId : this.userId,
-          password: this.password
-        }
-        const res = await axios.get('https://jsonplaceholder.typicode.com/users/', data);
-        this.data = res.data;
 
+  // composition API
+  setup() {
+    // data
+    const userId = ref('');
+    const password = ref('');
+    const data = ref([]);
+
+    // methods
+    const submit = async function() {
+      try {
+        const res = await axios.get('https://jsonplaceholder.typicode.com/users/', {
+          userId : userId.value,    //
+          password: password.value  //
+        });
+        this.data = res.data;
       } catch (e) {
         console.log(e);
       }
     }
-  }
+
+    return { userId, password, data, submit } // return 한 값들은 option에서 사용 가능
+  },
+
+  // 기존 option 설정
+  // data() {
+  //   return {
+  //     userId: '',
+  //     password: '',
+  //     data: [],
+  //   }
+  // },
+  // methods: {
+  //   async submit() {
+  //     try {
+  //       const data = {
+  //         userId : this.userId,
+  //         password: this.password
+  //       }
+  //       const res = await axios.get('https://jsonplaceholder.typicode.com/users/', data);
+  //       this.data = res.data;
+
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //   }
+  // }
 }
 </script>
 
