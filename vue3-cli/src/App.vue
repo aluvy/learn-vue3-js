c06a3893de7e2c4f47af7fab5e39ec5ccf57fb28
songmiok <aluvy_@naver.com>
2024-02-14 11:00:20 +0900

    Create App.vue

---
+++ vue3-cli/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <AppHeader :appTitle="appTitle" @changeTitle="changeTitle"></AppHeader>
+  <div>
+    {{ msg }}
+  </div>
+  <button type="button" @click="showAlert">alert</button>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+      msg: 'hi',
+      appTitle: '앱 헤더 컴포넌트'
+    }
+  },
+  methods: {
+    showAlert() {
+      alert('hi')
+    },
+    changeTitle() {
+      this.appTitle = '체인지'
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
